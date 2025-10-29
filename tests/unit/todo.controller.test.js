f2fddb4de71fff3b9d6b4fe0d92face828052533
kylakarla <karl.markus.saukand@gmail.com>
2025-10-29 14:58:54 +0200

    Created TodoController.getTodos unit test and integration tests

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -2,8 +2,10 @@ const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../models/todo.model");
 const httpMocks = require("node-mocks-http");
 const newTodo = require("../mock-data/new-todo.json");
+const allTodos = require("../mock-data/all-todos.json");
 
 TodoModel.create = jest.fn();
+TodoModel.find = jest.fn();
 
 let req, res, next;
 beforeEach(() => {
@@ -40,4 +42,29 @@ describe("TodoController.createTodo", () => {
     await TodoController.createTodo(req, res, next);
     expect(next).toHaveBeenCalledWith(errorMessage);
   });
+});
+
+describe("TodoController.getTodos", () => {
+  it("should have a getTodos function", () => {
+    expect(typeof TodoController.getTodos).toBe("function");
+  });
+  it("should call TodoModel.find({})", async () => {
+    await TodoController.getTodos(req, res, next);
+    expect(TodoModel.find).toHaveBeenCalledWith({});
+    });
+  it("should return 200 status code and all todos", async () => {
+    TodoModel.find.mockReturnValue(allTodos);
+    await TodoController.getTodos(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled()).toBeTruthy();
+    expect(res._getJSONData()).toStrictEqual(allTodos);
+  });
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Error finding todos" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    TodoModel.find.mockReturnValue(rejectedPromise);
+    await TodoController.getTodos(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+
 });
